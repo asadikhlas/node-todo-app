6d5feef6b16d9f684dc3f4fcd4e69df2600c9a3d
Asad Ikhlas <asadikhlas03@gmail.com>
2019-04-19 23:25:04 +0500

    install nodemon

--- playground/app.js
+++ playground/app.js
@@ -1,10 +1,11 @@
 const validator = require('validator')
 const printMsg =  require('./msg')
+const chalk = require('chalk')
 
 
 let msg = printMsg()
 console.log(msg)
-
+console.log(chalk.blue('hello'))
 const myEmail = "asadikhlas@gmail.com"
 
 console.log(validator.isEmail(myEmail))
